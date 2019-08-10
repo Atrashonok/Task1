259800f1d0c8f06e7ac8ce05e991ad101e275bc9
Atrashonok <436bgh151097@gmail.com>
2019-08-11 01:22:32 +0300

    transform to 1 string

--- lcs.js
+++ lcs.js
@@ -1,16 +1,1 @@
-if(process.argv.length<3){
-console.log('')
-}else{
-let s=process.argv.slice(2)
-let h=s.reduce((a,b)=>a.length<=b.length?a:b)
-let m=h.length
-outer:for(let l=m;l>0;l--){
-for(let t=0;t<=m-l;t++){
-let b=h.substring(t,t+l)
-if(s.every(e=>~e.indexOf(b))){
-console.log(b)
-break outer
-}
-}
-}
-}
+if(process.argv.length<3){console.log('');}else{let s=process.argv.slice(2);let h=s.reduce((a,b)=>a.length<=b.length?a:b);let m=h.length;outer:for(let l=m;l>0;l--){for(let t=0;t<=m-l;t++){let b=h.substring(t,t+l);if(s.every(e=>~e.indexOf(b))){console.log(b); break outer}}}}
